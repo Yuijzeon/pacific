283ccae7e40f67c2bb6e1222b54f3bb53665a3b5
llaurrrraa <85481566+llaurrrraa@users.noreply.github.com>
2021-10-10 17:26:05 +0800

    update post

---
+++ WebApplication/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult article()
+        {
+            return View();
+        }
+    }
+}
