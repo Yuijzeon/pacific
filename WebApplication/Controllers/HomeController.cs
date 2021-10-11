a762ad1b859e0452a16ee4ad3b2d3dd6519313a1
YUIJZEON <dingyujen@gmail.com>
2021-10-12 02:31:34 +0800

    Update Model 資料庫任務

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
@@ -11,7 +13,33 @@ namespace WebApplication.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            DataTable 查詢報表 = new 資料庫任務() {
+                查詢字串 = "USE teamdb2 SELECT * FROM Post;"
+            }.Get資料表();
+
+            ViewBag.Test = ConvertDataTableToHTML(查詢報表);
+
             return View();
+        }
+
+        public static string ConvertDataTableToHTML(DataTable dt)
+        {
+            string html = "<table>";
+            //add header row
+            html += "<tr>";
+            for (int i = 0; i < dt.Columns.Count; i++)
+                html += "<td>" + dt.Columns[i].ColumnName + "</td>";
+            html += "</tr>";
+            //add rows
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                html += "<tr>";
+                for (int j = 0; j < dt.Columns.Count; j++)
+                    html += "<td>" + dt.Rows[i][j].ToString() + "</td>";
+                html += "</tr>";
+            }
+            html += "</table>";
+            return html;
         }
     }
 }
