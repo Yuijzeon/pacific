f0fecf2fd80fb4f9ec94266e60a78334d5eda427
YUIJZEON <dingyujen@gmail.com>
2021-10-13 11:12:23 +0800

    Update 資料庫任務.cs

--- 第二組期末專題/Models/資料庫任務.cs
+++ 第二組期末專題/Models/資料庫任務.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
 
 namespace 第二組期末專題.Models
 {
     public class 資料庫任務
     {
+        public 資料庫任務() {}
+
+        public 資料庫任務(string str)
+        {
+            查詢字串 = str;
+        }
+
+
         public string 連線字串 = "Server=tcp:teamtwodb.database.windows.net,1433;" +
                                  "Initial Catalog=teamdb2;" +
                                  "Persist Security Info=False;" +
@@ -21,10 +25,11 @@ namespace 第二組期末專題.Models
 
         public string 查詢字串 { get; set; }
 
-        public 資料庫任務(string str)
-            {
-                查詢字串 = str;
-            }
+        public delegate SqlCommand 回呼指令(SqlCommand 指令);
+        public 回呼指令 注入參數 = delegate (SqlCommand 指令)
+        {
+            return 指令;
+        };
 
 
         public DataTable Get資料表()
@@ -33,8 +38,8 @@ namespace 第二組期末專題.Models
 
             using (SqlConnection 連線 = new SqlConnection(連線字串))
             {
-                SqlCommand 指令 = new SqlCommand(查詢字串, 連線);
                 連線.Open();
+                SqlCommand 指令 = 注入參數(new SqlCommand(查詢字串, 連線));
                 SqlDataAdapter 資料中繼器 = new SqlDataAdapter(指令);
                 資料中繼器.Fill(資料表);
             }
@@ -46,8 +51,8 @@ namespace 第二組期末專題.Models
         {
             using (SqlConnection 連線 = new SqlConnection(連線字串))
             {
-                SqlCommand 指令 = new SqlCommand(查詢字串, 連線);
                 連線.Open();
+                SqlCommand 指令 = 注入參數(new SqlCommand(查詢字串, 連線));
                 指令.ExecuteNonQuery();
             }
         }
