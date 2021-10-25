fdbc8cb7442ef4ee0cb5659ed7df43774d81a875
YUIJZEON <dingyujen@gmail.com>
2021-10-25 09:27:57 +0800

    直接 @Session 已登入的用戶名顯示在 navbar

--- 第二組期末專題/Controllers/HomeController.cs
+++ 第二組期末專題/Controllers/HomeController.cs
@@ -14,27 +14,6 @@ namespace 第二組期末專題.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            string 我的查詢 = "";
-            /*
-            DataTable 報表 = new 資料庫任務()
-            {
-                查詢字串 = 我的查詢,
-                注入參數 = (指令) =>
-                {   
-                    指令.Parameters.AddWithValue("@1", "AAA");
-                    指令.Parameters.AddWithValue("@2", "BBB");
-                    指令.Parameters.AddWithValue("@3", "CCC");
-                    return 指令;
-                }
-            }.Get資料表();
-            
-            new 任務Update<用戶>(new 用戶 {
-                Id = 6,
-                手機 = "0988888888",
-                註冊日期 = DateTime.Parse("2021-10-18 16:30:00")
-            }).SetBy欄位(new[] {"大頭貼", "手機" , "註冊日期" });
-            */
-            ViewBag.Name = Session["Name"];
             return View();
         }
 
