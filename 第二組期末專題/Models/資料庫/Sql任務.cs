151df7b89e65195923c357a7d877c8c210a2101c
YUIJZEON <dingyujen@gmail.com>
2021-11-03 09:35:46 +0800

    更新資料庫-更新.cs

---
+++ 第二組期末專題/Models/資料庫/Sql任務.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace 第二組期末專題
+{
+    public class Sql任務
+    {
+        //建構函式 開始
+        public Sql任務(string 查詢字串 = null, object Sql指令參數 = null)
+        {
+            this.連線字串 = "Server=tcp:datateam2.database.windows.net,1433;" +
+                            "Initial Catalog=team2;" +
+                            "Persist Security Info=False;" +
+                            "User ID=team2;" +
+                            "Password=XYZtwo22;" +
+                            "MultipleActiveResultSets=False;" +
+                            "Encrypt=True;" +
+                            "TrustServerCertificate=False;" +
+                            "Connection Timeout=30;";
+
+            this.查詢字串 = 查詢字串;
+
+            this.Sql指令參數 = Sql指令參數;
+
+            this.操作流程 = delegate (SqlCommand 指令)
+            {
+                指令.ExecuteNonQuery();
+            };
+        }
+        //建構函式 結束
+
+        //物件屬性 開始
+        public string 連線字串 { get; set; }
+
+        public string 查詢字串 { get; set; }
+
+        public object Sql指令參數 { get; set; }
+
+        public Action<SqlCommand> 操作流程 { get; set; }
+        //物件屬性 結束
+
+        //物件方法 開始
+        public void 執行()
+        {
+            using (SqlConnection 連線 = new SqlConnection(連線字串))
+            {
+                連線.Open();
+
+                SqlCommand 指令 = new SqlCommand(查詢字串, 連線);
+
+                if (Sql指令參數 != null)
+                {
+                    指令 = (Sql指令參數 is Dictionary<string, object>) ? 注入參數字典(指令) : 注入參數物件(指令);
+                }
+
+                this.操作流程(指令);
+
+                連線.Close();
+            }
+        }
+
+        private SqlCommand 注入參數字典(SqlCommand 指令)
+        {
+            foreach (var 欄位 in Sql指令參數 as Dictionary<string, object>)
+            {
+                string 注入鍵 = 欄位.Key;
+                object 注入值 = 欄位.Value;
+
+                if (注入值 == null) continue;
+
+                if (注入值.GetType() == typeof(DateTime))
+                {
+                    注入值 = ((DateTime)注入值).ToString("yyyy-MM-dd HH:mm:ss");
+                }
+
+                指令.Parameters.AddWithValue(注入鍵, 注入值);
+            }
+            return 指令;
+        }
+
+        private SqlCommand 注入參數物件(SqlCommand 指令)
+        {
+            foreach (PropertyInfo 屬性 in Sql指令參數.GetType().GetProperties())
+            {
+                string 注入鍵 = 屬性.Name;
+                object 注入值 = 屬性.GetValue(Sql指令參數);
+
+                if (注入值 == null) continue;
+
+                if (注入值.GetType() == typeof(DateTime))
+                {
+                    注入值 = ((DateTime)注入值).ToString("yyyy-MM-dd HH:mm:ss");
+                }
+
+                指令.Parameters.AddWithValue(注入鍵, 注入值);
+            }
+            return 指令;
+        }
+
+        public Sql任務 加入參數(object 參數鍵值)
+        {
+            this.Sql指令參數 = 參數鍵值;
+
+            return this;
+        }
+
+        public object 讀取()
+        {
+            object 查詢結果 = new object();
+
+            this.操作流程 = delegate (SqlCommand 指令)
+            {
+                查詢結果 = 指令.ExecuteScalar();
+            };
+
+            this.執行();
+
+            return 查詢結果;
+        }
+
+        public void 讀取(Action<SqlDataReader> When讀取到一筆資料)
+        {
+            this.操作流程 = delegate (SqlCommand 指令)
+            {
+                SqlDataReader 資料讀取器 = 指令.ExecuteReader();
+
+                while (資料讀取器.Read())
+                {
+                    When讀取到一筆資料(資料讀取器);
+                }
+            };
+
+            this.執行();
+        }
+    }
+}
--- 第二組期末專題/Models/資料庫/資料庫-讀取.cs
+++ 第二組期末專題/Models/資料庫/資料庫-讀取.cs
@@ -9,52 +9,31 @@ namespace 第二組期末專題
 {
     public static partial class 資料庫
     {
-        public static object 讀取格(string 查詢字串, object 欲注入鍵值 = null)
-        {
-            object 資料格 = new object();
-
-            new 讀取任務(查詢字串)
-            {
-                注入鍵值 = 欲注入鍵值,
-                操作流程 = delegate (SqlCommand 指令)
-                {
-                    資料格 = 指令.ExecuteScalar();
-                }
-            }.執行();
-
-            return 資料格;
-        }
-
-
         public static List<某類別> 讀取表<某類別>(string 查詢字串, object 欲注入鍵值 = null) where 某類別 : Dictionary<string, object>, new()
         {
             List<某類別> 字典清單 = new List<某類別>();
 
-            new 讀取任務(查詢字串)
+            new Sql任務(查詢字串, 欲注入鍵值).讀取(delegate (SqlDataReader 資料讀取器)
             {
-                注入鍵值 = 欲注入鍵值,
-                When讀取到一筆資料 = delegate (SqlDataReader 資料讀取器)
+                某類別 字典 = new 某類別();
+                for (int i = 0; i < 資料讀取器.FieldCount; i++)
                 {
-                    某類別 字典 = new 某類別();
-                    for (int i = 0; i < 資料讀取器.FieldCount; i++)
+                    string 欄位標題 = 資料讀取器.GetName(i);
+                    object 儲存格值 = 資料讀取器[i];
+
+                    if (儲存格值.GetType() == typeof(DateTime))
                     {
-                        string 欄位標題 = 資料讀取器.GetName(i);
-                        object 儲存格值 = 資料讀取器[i];
-
-                        if (儲存格值.GetType() == typeof(DateTime))
-                        {
-                            儲存格值 = ((DateTime)儲存格值).ToString("yyyy-MM-dd HH:mm:ss");
-                        }
-
-                        try // INNER JOIN 排除相同欄位(通常是ID)
-                        {
-                            字典.Add(欄位標題, 儲存格值);
-                        }
-                        catch { }
+                        儲存格值 = ((DateTime)儲存格值).ToString("yyyy-MM-dd HH:mm:ss");
                     }
-                    字典清單.Add(字典);
+
+                    try // INNER JOIN 排除相同欄位(通常是ID)
+                    {
+                        字典.Add(欄位標題, 儲存格值);
+                    }
+                    catch { }
                 }
-            }.執行();
+                字典清單.Add(字典);
+            });
 
             return 字典清單;
         }
@@ -74,19 +53,17 @@ namespace 第二組期末專題
             return 讀取表<某類別>(查詢字串, 注入鍵值);
         }
 
-        public static 某類別 讀取<某類別>(object Id) where 某類別 : Dictionary<string, object>, new()
+        public static 某類別 讀取<某類別>(object ID) where 某類別 : Dictionary<string, object>, new()
         {
             string 查詢字串 = $"SELECT TOP 1 * FROM [{typeof(某類別).Name}] WHERE [Id]=@ID";
 
-            object 注入鍵值 = new { ID = Id };
-
-            return 讀取表<某類別>(查詢字串, 注入鍵值).FirstOrDefault();
+            return 讀取表<某類別>(查詢字串, new { ID }).FirstOrDefault();
         }
 
 
 
         // 擴充功能區 待整理
-        public static List<某類別> GetBy<某類別>(this List<用戶Favorite> 用戶Favorite清單, string 外鍵名稱) where 某類別 : Dictionary<string, object>, new()
+        public static List<某類別> GetBy<某類別, 中繼類別>(this List<中繼類別> 用戶Favorite清單, string 外鍵名稱) where 某類別 : Dictionary<string, object>, new() where 中繼類別 : Dictionary<string, object>, new()
         {
             List<string> ID清單 = new List<string>();
 
