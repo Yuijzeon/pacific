7ebe1cc18d9c145edb9978f44ea3f9bb543b77c6
YUIJZEON <dingyujen@gmail.com>
2021-11-05 10:52:26 +0800

    修正筆誤

--- 第二組期末專題/Controllers/SearchController.cs
+++ 第二組期末專題/Controllers/SearchController.cs
@@ -58,9 +58,9 @@ namespace 第二組期末專題.Controllers
             string where = "/Search";
             where += (form["result"] != null) ? $"?result={form["result"]}" : "";
             where += (form["active"] != null) ? $"?active={form["active"]}" : "";
-            where += (form["starttime"] != null) ? $"?result={form["starttime"]}" : "";
-            where += (form["endtime"] != null) ? $"?result={form["endtime"]}" : "";
-            where += (form["point"] != null) ? $"?result={form["point"]}" : "";
+            where += (form["starttime"] != null) ? $"?starttime={form["starttime"]}" : "";
+            where += (form["endtime"] != null) ? $"?endtime={form["endtime"]}" : "";
+            where += (form["point"] != null) ? $"?point={form["point"]}" : "";
             return Redirect(where);
         }
     }
