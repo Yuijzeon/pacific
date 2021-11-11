529dda272e59e8850e4d181a97085c2428c1e6a4
Huan-Jun <89066641+Huan-Jun@users.noreply.github.com>
2021-11-11 18:50:20 +0800

    test完成

--- 第二組期末專題/Content/js/test.js
+++ 第二組期末專題/Content/js/test.js
@@ -21,21 +21,25 @@ function 生成用戶列表 (資料s) {
 
     for (var 資料 of 資料s) {
         var 資料列 = $('<tr>').attr('data-id', 資料['Id']);
-        for (var 欄位名稱 in 資料) {
-            資料列.append(
-                $('<td>').text(資料[欄位名稱])
-            );
-        }
+        if (資料['帳號'] != "0") {
+            for (var 欄位名稱 in 資料) {
 
-        資料列.append(
-            $('<td>').append(
-                $('<button>').attr({
-                    type: "button",
-                    class: "btn btn-danger"
-                }).text('🗑️').on('click', 刪除資料列)
-            )
-        );
-        資料表.append(資料列);
+                資料列.append(
+                    $('<td>').text(資料[欄位名稱])
+                );
+
+            }
+
+            資料列.append(
+                $('<td>').append(
+                    $('<button>').attr({
+                        type: "button",
+                        class: "btn btn-danger"
+                    }).text('🗑️').on('click', 刪除資料列)
+                )
+            );
+            資料表.append(資料列);
+        }
     }
     var 標題列 = $('<tr>');
     for (var 欄位名稱 in 資料s[0]) {
@@ -63,12 +67,20 @@ function 生成用戶列表 (資料s) {
 }
 
 function 刪除資料列() {
-    var 資料列 = $(this).closest('tr');
-    $.ajax({
-        type: 'GET',
-        url: '../Test/DeleteUser?id=' + 資料列.attr('data-id')
-    }).done(function (新資料s) {
-        $('#showAllUserData').html('');
-        生成用戶列表(新資料s)
-    });
+    var yes = confirm("確定刪除此筆資料?")
+    if (yes) {
+        var 資料列 = $(this).closest('tr');
+        $.ajax({
+            type: 'POST',
+            url: '../Test/DeleteUser',
+            //?id=' + 資料列.attr('data- id')
+            data: 'id=' + 資料列.attr('data-id'),
+        }).done(function (新資料s) {
+            $('#showAllUserData').html('');
+            生成用戶列表(新資料s);
+
+            alert("資料刪除成功!!");
+            document.location.href = "/Test";
+        });
+    }
 }
--- 第二組期末專題/Controllers/TestController.cs
+++ 第二組期末專題/Controllers/TestController.cs
@@ -27,11 +27,21 @@ namespace 第二組期末專題.Controllers
             return Json(全部用戶, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult DeleteUser(int id)
+        [HttpPost]
+        public ActionResult DeleteUser(string id)
         {
-            new 資料庫任務("DELETE FROM [文章] WHERE [Id]=@ID").注入參數by(new { ID = id }).Set();
-
-            var 全部用戶 = 資料庫.讀取<文章>();
+            var 全部用戶 = 資料庫.讀取<用戶>();
+            用戶 指定用戶 = new 用戶();
+            if (id == null)
+            {
+                return Json(全部用戶, JsonRequestBehavior.AllowGet);
+            }
+            指定用戶["Id"] = id;
+            指定用戶["帳號"] = "0";
+            指定用戶["密碼"] = "0";
+            指定用戶["名字"] = "0";
+            指定用戶["手機"] = "0";
+            (new 用戶CRUD()).更新(指定用戶);
             return Json(全部用戶, JsonRequestBehavior.AllowGet);
         }
     }
