6ded775a26cefe6752384664ce2c407b4f95f3e9
YUIJZEON <dingyujen@gmail.com>
2021-11-14 21:43:56 +0800

    首頁旅程

--- GoGoShare/Controllers/ListController.cs
+++ GoGoShare/Controllers/ListController.cs
@@ -50,7 +50,7 @@ namespace GoGoShare.Controllers
                 文章.用戶.Clear();
                 文章.評級.Clear();
 
-                刪除任務.文章.Remove(刪除任務.文章.Find(id));
+                刪除任務.文章.Remove(文章);
                 刪除任務.SaveChanges();
             }
             catch (Exception e)
@@ -61,11 +61,34 @@ namespace GoGoShare.Controllers
             return null;
         }
 
-        public JsonResult MyAllPosts()
+        public string DeleteUser(int id)
         {
-            var result = new SQL任務($"SELECT * FROM [文章] WHERE [作者用戶_FK]={Session["ID"]}").讀取();
-            
-            return Json(result, JsonRequestBehavior.AllowGet);
+            try
+            {
+                SQL任務 刪除任務 = new SQL任務();
+                用戶 用戶 = 刪除任務.用戶.Find(id);
+
+                用戶.Hashtag.Clear();
+                用戶.旅程包.Clear();
+                用戶.文章.Clear();
+                用戶.評級.Clear();
+
+                刪除任務.用戶.Remove(用戶);
+                刪除任務.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+            return null;
         }
+
+        //public JsonResult MyAllPosts()
+        //{
+        //    var result = new SQL任務($"SELECT * FROM [文章] WHERE [作者用戶_FK]={Session["ID"]}").讀取();
+
+        //    return Json(result, JsonRequestBehavior.AllowGet);
+        //}
     }
 }
