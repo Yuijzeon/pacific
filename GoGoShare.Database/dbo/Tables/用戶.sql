8f6d532abf52b1827b53e037ce0cf5603b399a55
Tintin Ting <yuijuhn.ting@titansoft.com.sg>
2024-01-24 23:44:32 +0800

    feat: add database project

---
+++ GoGoShare.Database/dbo/Tables/用戶.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[用戶](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[帳號] [nvarchar](50) NOT NULL,
+	[密碼] [nvarchar](50) NOT NULL,
+	[名字] [nvarchar](50) NOT NULL,
+	[手機] [nvarchar](50) NOT NULL,
+	[註冊日期] [nvarchar](50) NOT NULL,
+	[大頭貼] [nvarchar](max) NOT NULL,
+	[點數] [int] NOT NULL,
+ CONSTRAINT [PK_用戶] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
