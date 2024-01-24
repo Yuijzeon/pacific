2d74a0621032f5b1fed17fce1a9adcbd00d548c6
Tintin Ting <yuijuhn.ting@titansoft.com.sg>
2024-01-24 22:51:46 +0800

    feat: add swagger

--- GoGoShare.Web/Program.cs
+++ GoGoShare.Web/Program.cs
@@ -1,16 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
